916af1a6c40e1d467348ce1e1ad0087d8804c9a0
Gonzalo Ezequiel <gep.perez@gmail.com>
2024-10-02 17:43:27 -0300

    02/10

--- clase06/EjemploClase/db/AccesoDatos.php
+++ clase06/EjemploClase/db/AccesoDatos.php
@@ -1,44 +1,44 @@
 <?php
 
-class AccesoDatos {
-
-    private static $_ObjetoAccesoDatos;
-    private $_objetoPDO;
-
-    private function __construct() {
-        try {
-            $this->_objetoPDO = new PDO('mysql:host=lolcalhost;dbname=clase06;charset=utf8', 'root', '', array(PDO::AT));
-            $this->_objetoPDO->exec("SET CHARACTER SET utf8");
+class AccesoDatos
+{
+    private static $ObjetoAccesoDatos;
+    private $objetoPDO;
+ 
+    private function __construct()
+    {
+        try { 
+            $this->objetoPDO = new PDO('mysql:host=localhost;dbname=clase06;charset=utf8', 'root', '', array(PDO::ATTR_EMULATE_PREPARES => false,PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+            $this->objetoPDO->exec("SET CHARACTER SET utf8");
+            } 
+        catch (PDOException $e) { 
+            print "Error!: " . $e->getMessage(); 
+            die();
         }
-        catch(PDOException $e) {
-            echo "Error: ".$e->getMessage();
-        }
+    }
+ 
+    public function RetornarConsulta($sql)
+    { 
+        return $this->objetoPDO->prepare($sql); 
     }
 
-    public function RetornarConsulta($sql) {
-        
-        return $this->_objetoPDO->prepare($sql);
+     public function RetornarUltimoIdInsertado()
+    { 
+        return $this->objetoPDO->lastInsertId(); 
     }
-
-    public function RetornarUltimoIdInsertado () {
-        
-        return $this->_objetoPDO->lastInsertId();
+ 
+    public static function dameUnObjetoAcceso()
+    { 
+        if (!isset(self::$ObjetoAccesoDatos)) {          
+            self::$ObjetoAccesoDatos = new AccesoDatos(); 
+        } 
+        return self::$ObjetoAccesoDatos;        
     }
-
-    /**
-     * Se asegura de que exista una única instancia creada del túnel | PATRON SINGLETON
-     */
-    public static function dameUnObjetoAcceso() {
-
-        if(!isset(self::$_ObjetoAccesoDatos)) {
-            self::$_ObjetoAccesoDatos = new AccesoDatos();
-        }
-        return self::$_ObjetoAccesoDatos;
+ 
+     // Evita que el objeto se pueda clonar
+    public function __clone()
+    { 
+        trigger_error('La clonación de este objeto no está permitida', E_USER_ERROR); 
     }
-
-    // Evita que el objeto se pueda clonar
-    public function __clone() {
-        
-    }
-
 }
+?>
