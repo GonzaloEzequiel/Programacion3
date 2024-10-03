891a7d9ca14dcb42ac457bf423f6b0f745ecb094
Gonzalo Ezequiel <gep.perez@gmail.com>
2024-10-03 15:19:52 -0300

    3/10

--- clase04/EjemploClase/json.php
+++ clase04/EjemploClase/json.php
@@ -1,29 +1,33 @@
 <?php
-// Array Indexado PHP a JSON
+// Array Indexado PHP a JSON -> se tranforma en un array
 $array = array("Jorge", "Hernan", "Pedro", "Marta");
 $json = json_encode($array);
 
 echo $json;
+echo "<br>";
 
 
-// Array asoc PHP a JSON
+// Array Asociativo PHP a JSON -> se tranforma en un Objeto
 $array_asoc = array("nombre" => "Jorge", "edad" => 32);
 $json_b = json_encode($array_asoc);
 
 echo $json_b;
+echo "<br>";
 
 
 // Clase/OBJ PHP a JSON
 $clase = new StdClass();
+$clase->variableCualquiera = true;
 $clase->nombre = 'Jorge';
 $clase->edad = 32;
 
 $json_c = json_encode($clase);
 
 echo $json_c;
+echo "<br>";
 
 
-// JSON a PHP
+// JSON a PHP (string a objeto)
 $json_obj = '{"nombre":"Pedro", "edad":37}';
 $obj = json_decode($json_obj);
 
